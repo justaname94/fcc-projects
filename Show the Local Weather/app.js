d555657ad26001ac9f4a68294c350da6ecc03d82
Roniel Valdez <roniel_valdez@outlook.com>
2016-11-01 18:22:24 -0400

    Added angular controller logic

--- Show the Local Weather/app.js
+++ Show the Local Weather/app.js
@@ -1,11 +1,27 @@
 (function() {
   var app = angular.module('weather', []);
 
-  app.controller('weatherController', function() {
+  app.controller('cardController', ['$scope', '$http', function($scope, $http) {
+    $scope.weathers = [];
+    $scope.weather = {};
+      getGeoCoords(function(coords) {
+      var apiKey = 'ce8874013ab7f94c2ac981a7bd8f2664';
+      var reqWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + coords.lat +
+      '&lon=' + coords.lng + '&units=metric'+ '&appid=' + apiKey;
+      $http.get(reqWeatherUrl).success(function(data) {
+        console.log(data);
+        $scope.weather.name = data.name;
+        $scope.weather.country = data.sys.country;
+        $scope.weather.temperature = data.main.temp;
+        $scope.weather.description = data.weather[0].description;
+        $scope.weather.humidity = data.main.humidity;
+        $scope.weather.wind_speed = data.wind.speed;
+        $scope.weathers.push($scope.weather);
+      });
+    });
 
-  });
+  }]);
 
-  app.directive('weatherLayout', function() {
-
-  });
 }());
+
+// My API Key: ce8874013ab7f94c2ac981a7bd8f2664
--- Show the Local Weather/script.js
+++ Show the Local Weather/script.js
@@ -1,3 +1,21 @@
-var weatherObj = {
+// Get Geographic coordinates of the user if
+// its browser supports the, otherwise display
+// NYC coordinates
+function getGeoCoords(cb) {
+  if(Modernizr.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      return cb({
+        supportGeo: true,
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+    });
+  } else {
+    return cb({
+      supportGeo: false,
+      lat: 40.7128,
+      lng: 40.7128
+    });
+  }
+}
 
-};
