bc4054661818a29a4e0bf5c511a94fa55debf024
Roniel Valdez <roniel_valdez@outlook.com>
2016-11-02 17:37:58 -0400

    Add possibility to change between C and F

--- Show the Local Weather/app.js
+++ Show the Local Weather/app.js
@@ -5,8 +5,9 @@
     return {
       restrict: 'E',
       templateUrl: 'weatherCard.html',
+
       controller: ['$scope', '$http', function($scope, $http) {
-        $scope.weathers = [];
+        // $scope.weathers = [];
         $scope.weather = {};
         getGeoCoords(function(coords) {
           var apiKey = 'ce8874013ab7f94c2ac981a7bd8f2664';
@@ -15,19 +16,35 @@
           $http.get(reqWeatherUrl).success(function(data) {
           $scope.weather.name = data.name;
           $scope.weather.country = data.sys.country;
-          $scope.weather.temperature = data.main.temp;
+          $scope.weather.temp = data.main.temp;
           $scope.weather.icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
           $scope.weather.description = data.weather[0].description;
           $scope.weather.humidity = data.main.humidity;
           $scope.weather.wind_speed = data.wind.speed;
-          $scope.weathers.push($scope.weather);
-      });
-    });
+          $scope.weather.scale = '°C';
+          //  $scope.weathers.push($scope.weather);
+          });
+        });
+
+        $scope.celsiusToFarenheit = function() {
+          if($scope.weather.scale === '°C') {
+            $scope.weather.scale = '°F';
+            $scope.weather.temp =
+               ((parseFloat($scope.weather.temp) * 9) / 5+32).toFixed(2);
+          }
+        };
+
+        $scope.farenheitToCelsius = function() {
+          if($scope.weather.scale === '°F') {
+            $scope.weather.scale = '°C';
+            $scope.weather.temp =
+               ((parseFloat($scope.weather.temp) - 32) * 5/9).toFixed(2);
+          }
+        };
+
       }],
       controllerAs: 'weather'
     };
   });
 
 }());
-
-// My API Key: ce8874013ab7f94c2ac981a7bd8f2664
