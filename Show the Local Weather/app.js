184272f4877ac8d28307d118a0b56b878c563406
Roniel Valdez <roniel_valdez@outlook.com>
2016-11-03 19:10:48 -0400

    move controller code logic to an angular service

--- Show the Local Weather/app.js
+++ Show the Local Weather/app.js
@@ -1,7 +1,21 @@
 (function() {
   var app = angular.module('weather', []);
 
-  app.directive('weatherCard', function() {
+  app.service('weatherService', function($http) {
+    this.getLocation = function() {
+      return $http.jsonp('http://ipinfo.io/json?callback=JSON_CALLBACK');
+    };
+
+    this.getWeatherData = function(city) {
+      var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+      var units = '&units=metric';
+      var appId = '&appid=ce8874013ab7f94c2ac981a7bd8f2664';
+      var cb = '&callback=JSON_CALLBACK';
+      return $http.jsonp(api + city + units + appId + cb);
+    };
+  });
+
+  app.directive('weatherCard', function(weatherService) {
     return {
       restrict: 'E',
       templateUrl: 'weatherCard.html',
@@ -9,22 +23,26 @@
       controller: ['$scope', '$http', function($scope, $http) {
         // $scope.weathers = [];
         $scope.weather = {};
-        getGeoCoords(function(coords) {
-          var apiKey = 'ce8874013ab7f94c2ac981a7bd8f2664';
-          var reqWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + coords.lat +
-          '&lon=' + coords.lng + '&units=metric'+ '&appid=' + apiKey;
-          $http.get(reqWeatherUrl).success(function(data) {
-          $scope.weather.name = data.name;
-          $scope.weather.country = data.sys.country;
+
+        weatherService.getLocation().success(function(data) {
+          var city = data.city + ',' + data.country;
+          $scope.weather.city = data.city;
+          $scope.weather.country = data.country;
+
+          weatherService.getWeatherData(city).success(function(data) {
+            ShowWeather(data);
+          });
+        });
+
+        function ShowWeather(data) {
           $scope.weather.temp = data.main.temp;
-          $scope.weather.icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+          $scope.weather.icon = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
           $scope.weather.description = data.weather[0].description;
           $scope.weather.humidity = data.main.humidity;
           $scope.weather.wind_speed = data.wind.speed;
           $scope.weather.scale = '°C';
           //  $scope.weathers.push($scope.weather);
-          });
-        });
+        }
 
         $scope.celsiusToFarenheit = function() {
           if($scope.weather.scale === '°C') {
