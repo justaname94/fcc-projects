6735ead0dce1a71d3cecbe827696a98eb984bcdc
Roniel Valdez <roniel_valdez@outlook.com>
2016-11-07 20:35:20 -0400

    Project structre refactor and non working version of autocomplete

---
+++ Show the Local Weather/src/app.js
@@ -0,0 +1,98 @@
+(function() {
+  var app = angular.module('weather', ['ngMaterial']);
+
+  app.run(function($rootScope) {
+    $rootScope.weathers = {};
+  });
+
+  app.service('weatherService', function($http, $rootScope, $timeout) {
+    var self = this;
+
+    self.getLocation = function() {
+      return $http.jsonp('http://ipinfo.io/json?callback=JSON_CALLBACK');
+    };
+
+    self.getWeatherData = function(city) {
+      var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+      var units = '&units=metric';
+      var appId = '&appid=ce8874013ab7f94c2ac981a7bd8f2664';
+      var cb = '&callback=JSON_CALLBACK';
+      return $http.jsonp(api + city + units + appId + cb);
+    };
+
+    self.showWeather = function(data) {
+      $rootScope.$apply(function() {
+        var weather = {};
+        weather.city = data.name;
+        weather.country = data.sys.country;
+        weather.temp = data.main.temp;
+        weather.icon = 'https://openweathermap.org/img/w/' +
+          data.weather[0].icon + '.png';
+        weather.description = data.weather[0].description;
+        weather.humidity = data.main.humidity;
+        weather.wind_speed = data.wind.speed;
+        weather.scale = '°C';
+        weather.scale_name = 'Farenheit';
+        $rootScope.weathers[Date.now()] = weather;
+      });
+    };
+
+    self.loadWeatheFromCity = function(city) {
+      self.getWeatherData(city).success(function(data) {
+        $timeout(function() {
+          self.showWeather(data);
+        });
+      });
+    };
+
+  });
+
+  app.controller('weatherController', ['$scope', 'weatherService',
+  function($scope, weatherService) {
+
+    weatherService.getLocation().success(function(data) {
+      var city = data.city + ',' + data.country;
+      weatherService.loadWeatheFromCity(city);
+    });
+
+    $scope.switchBetweenCandF = function(weatherCard) {
+      if(weatherCard.scale === '°C') {
+        weatherCard.scale = '°F';
+        weatherCard.temp =
+            ((parseFloat(weatherCard.temp) * 9) / 5+32).toFixed(2);
+        weatherCard.scale_name = 'Celsius';
+      } else {
+        weatherCard.scale = '°C';
+        weatherCard.temp =
+            ((parseFloat(weatherCard.temp) - 32) * 5/9).toFixed(2);
+        weatherCard.scale_name = 'Farenheit';
+      }
+    };
+  }]);
+
+  app.controller('AutoCompleteController', ['$http', '$scope', 'weatherService',
+  function($http, $scope, weatherService) {
+    self = this;
+
+    // Internal Methods
+    function loadAll() {
+      return $http.get('http://api.myjson.com/bins/3mhlk');
+    }
+
+  }]);
+
+  app.directive('weatherCard', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'weatherCardMaterial.html',
+    };
+  });
+
+  app.directive('autocompleteBar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'autocompleteBar.html'
+    };
+  });
+
+}());
