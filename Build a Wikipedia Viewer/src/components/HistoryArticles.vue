5449ff7764cd33197532c08325178d00cda1a2e9
Roniel Valdez <roniel_valdez@outlook.com>
2016-12-18 11:18:33 -0400

    Added routing

---
+++ Build a Wikipedia Viewer/src/components/HistoryArticles.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <ul class="list-unstyled wiki-article">
+        <transition-group name="fade-down-animation"
+          enter-active-class="animated fadeInDown"
+          tag="a"
+        >
+          <a v-for="article in filteredArticles" v-bind:href="article.link" v-bind:key="article.title" target="_blank">
+            <li class="col-md-10 col-md-offset-1">
+              <i class="remove-article fa fa-minus fa-lg" aria-hidden="true"></i>
+              <h4 class=""><strong>{{ article.title }}</strong></h4>
+              <h5>{{ article.description }}</h5>
+            </li>
+          </a>
+        </transition-group>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import bus from '../bus'
+</script>
