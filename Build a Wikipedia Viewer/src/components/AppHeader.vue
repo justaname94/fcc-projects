49f38259b3763a6da2311d1c80ea13ea71e2b221
Roniel Valdez <roniel_valdez@outlook.com>
2016-12-18 20:31:47 -0400

    added darker background to history router active

--- Build a Wikipedia Viewer/src/components/AppHeader.vue
+++ Build a Wikipedia Viewer/src/components/AppHeader.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <!--<a class="navbar-brand nav-link" href="#">Search</a>-->
         <h4 class="navbar-text"><router-link to="/">Wikipedia Viewer</router-link></h4>
-        <router-link to="/history" class="navbar-brand navbar-right nav-link" href="#">History</router-link>
+        <router-link to="/history" class="history navbar-brand navbar-right nav-link" href="#">History</router-link>
       </div>
     </nav>
   </header>
